ea66424db6296c0b64a5b326a524f04f6ae58dd3
gangadhargo <gangadhar.reddy7@gmail.com>
2017-03-21 11:02:31 +0530

    Add files via upload

---
+++ Algorithms/CamelCase.java
@@ -0,0 +1,18 @@
+package sample;
+
+import java.util.Scanner;
+
+public class CamelCase {
+
+	public static void main(String[] args) {
+		int count = 1;
+		Scanner scanner = new Scanner(System.in);
+		String string = scanner.next();
+		for (int i = 0; i < string.length(); i++) {
+			if (Character.isUpperCase(string.charAt(i))) {
+				count = count + 1;
+			}
+		}
+		System.out.println(count);
+	}
+}
